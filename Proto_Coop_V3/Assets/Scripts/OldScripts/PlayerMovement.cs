2ca694fe09b3f37b55a51e73e73cdd633e3d5f49
TeddyBeast <61504758+TeddyBeast@users.noreply.github.com>
2020-03-14 14:08:21 +0100

    Refresh Proto

--- Proto_Coop_V3/Assets/Scripts/OldScripts/PlayerMovement.cs
+++ Proto_Coop_V3/Assets/Scripts/OldScripts/PlayerMovement.cs
@@ -6,49 +6,96 @@ public class PlayerMovement : MonoBehaviour
 {
     public int nbreGamepadConnected;
 
-    public float speed = 7f;
-    public float powerJump = 7f;
-    public int Player;
-    public bool isJumping = true;
-    public bool canJump = true;
+    public Transform groundCheck;
 
-    private float h;
-    private float v;
-
-    private float distToGround;
-    private Rigidbody rb;
+    public LayerMask groundMask;
 
     private Vector3 InitScale;
+
+    Vector3 velocity;
+
+    private Rigidbody rb;
+
+    private float moveH;
+    private float moveV;
+
+    public int Player;
+
+    public float speed = 7f;
+    public float groundDistance = 1f;
+    public float JumpHeight = 7f;
+    public float gravity = -9.81f;
+
+    public bool isGrounded;
+
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        distToGround = GetComponent<Collider>().bounds.extents.y;
-        nbreGamepadConnected = Input.GetJoystickNames().Length;
-        print("Number of gamepad connected : " + nbreGamepadConnected);
+        groundCheck = GetComponent<Transform>();
+        velocity = rb.velocity;
 
         InitScale = transform.localScale;
     }
 
     private void FixedUpdate()
     {
-        if (nbreGamepadConnected == 2)
+        if (Input.GetAxis("HorizontalPlayer" + Player) != 0)
         {
-            Controls2Gamepads();
+            moveH = Input.GetAxis("HorizontalPlayer" + Player);
+        }
+        else if (Input.GetAxis("HorizontalPlayer" + Player) == 0)
+        {
+            moveH = 0f;
+        }
+        if (hinput.gamepad[Player].leftStick.horizontal != 0)
+        {
+            moveH = hinput.gamepad[Player].leftStick.horizontal;
         }
 
-        if (nbreGamepadConnected == 1)
+        if (Input.GetAxis("VerticalPlayer" + Player) != 0)
         {
-            ControlsGamepadKeyboard();
+            moveV = Input.GetAxis("VerticalPlayer" + Player);
+        }
+        else if (Input.GetAxis("HorizontalPlayer" + Player) == 0)
+        {
+            moveV = 0f;
+        }
+        if (hinput.gamepad[Player].leftStick.vertical != 0)
+        {
+            moveV = hinput.gamepad[Player].leftStick.vertical;
         }
 
-        if (nbreGamepadConnected == 0)
+        if (Input.GetButtonDown("JumpPlayer" + Player) || hinput.gamepad[Player].A.justPressed)
         {
-            ControlsKeyboards();
+            Jump();
         }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+
+        Vector3 desiredMove = (transform.forward * moveV) + (transform.right * moveH);
+        desiredMove = Vector3.ClampMagnitude(desiredMove, 1f);
+
+        Vector3 targetPos = transform.position + desiredMove * speed * Time.deltaTime;
+
+        rb.MovePosition(targetPos);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        rb.velocity = velocity;
+
+
 
         if (hinput.gamepad[0].B.justPressed && Player == 0)
         {
-            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y / 2 , transform.localScale.z);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y / 2, transform.localScale.z);
         }
         else if (hinput.gamepad[0].B.justReleased && Player == 0)
         {
@@ -56,120 +103,14 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void ControlsKeyboards()
-    {
-        if (Player == 0)
-        {
-            h = Input.GetAxis("HorizontalPlayer1");
-            v = Input.GetAxis("VerticalPlayer1");
-        }
-        else if (Player == 1)
-        {
-            h = Input.GetAxis("HorizontalPlayer2");
-            v = Input.GetAxis("VerticalPlayer2");
-        }
-
-
-        Vector3 desiredMove = (transform.forward * v) + (transform.right * h);
-        desiredMove = Vector3.ClampMagnitude(desiredMove, 1f);
-
-        Vector3 targetPos = transform.position + desiredMove * speed * Time.deltaTime;
-
-        rb.MovePosition(targetPos);
-
-        bool isGrounded = Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
-
-        if (isGrounded == true)
-        {
-            isJumping = false;
-        }
-        if (Player == 0)
-        {
-            if (Input.GetButtonDown("JumpPlayer1") && isJumping == false && canJump == true)
-            {
-                Jump();
-            }
-        }
-        if (Player == 1)
-        {
-            if (Input.GetButtonDown("JumpPlayer2") && isJumping == false && canJump == true)
-            {
-                Jump();
-            }
-        }
-    }
-
-    private void ControlsGamepadKeyboard()
-    {
-        if (Player == 0)
-        {
-            h = hinput.gamepad[Player].leftStick.horizontal;
-            v = hinput.gamepad[Player].leftStick.vertical;
-        }
-        if (Player == 1)
-        {
-            h = Input.GetAxis("HorizontalPlayer2");
-            v = Input.GetAxis("VerticalPlayer2");
-        }
-
-
-        Vector3 desiredMove = (transform.forward * v) + (transform.right * h);
-        desiredMove = Vector3.ClampMagnitude(desiredMove, 1f);
-
-        Vector3 targetPos = transform.position + desiredMove * speed * Time.deltaTime;
-
-        rb.MovePosition(targetPos);
-
-        bool isGrounded = Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
-
-        if (isGrounded == true)
-        {
-            isJumping = false;
-        }
-        if (hinput.gamepad[Player].A.pressed && isJumping == false && canJump == true)
-        {
-            Jump();
-        }
-
-        if (Player == 1)
-        {
-            if (Input.GetButtonDown("JumpPlayer2") && isJumping == false && canJump == true)
-            {
-                Jump();
-            }
-        }
-    }
-
-    private void Controls2Gamepads()
-    {
-        h = hinput.gamepad[Player].leftStick.horizontal;
-        v = hinput.gamepad[Player].leftStick.vertical;
-
-        Vector3 desiredMove = (transform.forward * v) + (transform.right * h);
-        desiredMove = Vector3.ClampMagnitude(desiredMove, 1f);
-
-        Vector3 targetPos = transform.position + desiredMove * speed * Time.deltaTime;
-
-        rb.MovePosition(targetPos);
-
-        bool isGrounded = Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
-
-        if (isGrounded == true)
-        {
-            isJumping = false;
-        }
-        if (hinput.gamepad[Player].A.pressed && isJumping == false && canJump == true)
-        {
-            Jump();
-        }
-    }
 
     private void Jump()
     {
-        print("Press jump");
-        Vector3 vel = rb.velocity;
-        vel.y = powerJump;
-        rb.velocity = vel;
-        isJumping = true;
+        if (isGrounded)
+        {
+            print("Press jump");
+            velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+            rb.velocity = velocity;
+        }
     }
 }
