ad54e9b406c7a32a654843ea9bb9eca2bd68aee3
NBohere <61057271+NBohere@users.noreply.github.com>
2020-03-14 14:55:30 +0100

    Import
    Grapple
    Clone
    Grappable Objects

---
+++ Proto_Coop_V3/Assets/Scripts/Powers/Clone.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clone : MonoBehaviour
+{
+    public Rigidbody rb;
+    public GameObject cloneGO;
+    public Vector3 playerVelocity;
+    private GameObject clone;
+    public bool cloned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) & cloned)
+        {
+            Destroy(clone);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            clone = Instantiate(cloneGO, transform.position, transform.rotation);
+            //playerVelocity = rb.velocity;
+            //clone.GetComponent<Rigidbody>().AddForce(playerVelocity * 2 + Vector3.up * 5, ForceMode.VelocityChange);
+            //clone.GetComponent<BoxCollider>().enabled = false;
+            //StartCoroutine(RebuildCollision());
+            cloned = true;
+        }
+        if (Input.GetKeyDown(KeyCode.R) & cloned)
+        {
+            transform.position = clone.transform.position;
+            Destroy(clone);
+            cloned = false;
+        }
+    }
+
+    //IEnumerator RebuildCollision()
+    //{
+    //    if (cloned)
+    //    {
+    //        yield return new WaitForSeconds(0.01f);
+    //        clone.GetComponent<BoxCollider>().enabled = true;
+    //    }
+    //}
+}
---
+++ Proto_Coop_V3/Assets/Scripts/Powers/Grapple.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+    public GameObject hookStart;
+
+    public GameObject hookEnd;
+    public Camera cameraMire;
+    public bool targeted = false;
+    private GameObject HookEndGO;
+    private Rigidbody rb;
+    public LineRenderer LR;
+    public SpringJoint Rope;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Fire1") & targeted)
+        {
+            //Debug.Log("Shooting");
+            ShootHook();
+        }
+        else
+        {
+            Vector3 forward = hookStart.transform.TransformDirection(Vector3.forward) * 1000;
+
+            Debug.DrawRay(hookStart.transform.position, forward, Color.green);
+
+            RaycastHit hit;
+            if (Physics.Raycast(hookStart.transform.position, forward, out hit) && (hit.transform.tag == "Grap"))
+            {
+                if (!targeted)
+                {
+                    HookEndGO = Instantiate(hookEnd, hit.point, Quaternion.identity);
+                    targeted = true;
+                }
+                else
+                {
+                    HookEndGO.transform.position = hit.point;
+                }
+
+            }
+            else
+            {
+                //targeted = false;
+                //Destroy(HookEndGO);
+            }
+        }
+
+        if (Input.GetButton("Fire2") & targeted)
+        {
+            targeted = false;
+            Destroy(HookEndGO);
+            //LR.enabled = false;
+            LR.positionCount = 0;
+            Rope = null;
+            Destroy(GetComponent<SpringJoint>());
+        }
+    }
+
+    public void ShootHook()
+    {
+        Destroy(Rope);
+        transform.position = Vector3.MoveTowards(transform.position, HookEndGO.transform.position, 1);
+        //LR.enabled = true;
+        LR.positionCount = 2;
+        LR.SetPosition(0, transform.position);
+        LR.SetPosition(1, HookEndGO.transform.position);
+
+        Rope = gameObject.AddComponent<SpringJoint>();
+        //newRope.enableCollision = false;
+        Rope.autoConfigureConnectedAnchor = false;
+        Rope.connectedAnchor = HookEndGO.transform.position;
+        Rope.spring = 6.5f;
+        Rope.damper = 2f;
+        //GameObject.DestroyImmediate(Rope);
+        //Rope = newRope;
+    }
+}
