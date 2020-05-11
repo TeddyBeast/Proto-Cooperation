0ecb6e0c6087ce6d3bc84c7d47c2e3b7890663b7
TeddyBeast <61504758+TeddyBeast@users.noreply.github.com>
2020-05-12 00:09:34 +0200

    Merge Mecas Sur la scène Game

---
+++ Proto_Coop_V3/Assets/Scripts/Debugs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugs : MonoBehaviour
+{
+    void Update()
+    {
+        // Restart Scene
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Proto_Coop_V3/Assets/Scripts/TeleporteurFinNiveau.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporteurFinNiveau : MonoBehaviour
+{
+    public GameObject PointPlayer1;
+    public GameObject PointPlayer2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player 1"))
+        {
+            other.transform.position = PointPlayer1.transform.position;
+        }
+
+        if (other.gameObject.CompareTag("Player 2"))
+        {
+            other.transform.position = PointPlayer2.transform.position;
+        }
+    }
+}
