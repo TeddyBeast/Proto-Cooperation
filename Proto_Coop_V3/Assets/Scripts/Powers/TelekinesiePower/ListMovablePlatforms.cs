bc1035dad9ac2b666412f877820a5131328c6554
NBohere <61057271+NBohere@users.noreply.github.com>
2020-05-27 21:46:44 +0200

    TEST_DO_NOT_TOUCH

--- Proto_Coop_V3/Assets/Scripts/Powers/TelekinesiePower/ListMovablePlatforms.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Sirenix.OdinInspector;
-
-public class ListMovablePlatforms : MonoBehaviour
-{
-    public List<GameObject> PlateformMovableTelekinesie = new List<GameObject>();
-}
